b4c47e86cc7788e4d4ba9e3fe928f66390d29d72
Shoi Hasegawa <wdlove1008sekai@gmail.com>
2018-10-14 19:20:57 +0900

    fix

--- php/PHP_LARAVEL.php
+++ php/PHP_LARAVEL.php
@@ -42,11 +42,13 @@ class Number02
     private function TechBoost()
     {
         $tech = "tech";
+        $tech .= " boost";
 
-        echo $tech." boost".PHP_EOL;  
+        echo $tech.PHP_EOL;  
     }
 
-    private function Show12(){
+    private function Show12()
+    {
         $calendar = [
             "January" => "1月",
             "February" => "2月",
@@ -88,9 +90,9 @@ class NUMBER03
         $name = "shoi";
         /* $name = "no";*/
     
-        if($name == "shoi"){
+        if ($name == "shoi") {
             echo "私は" . $name . " です。".PHP_EOL;
-        }else{
+        } else {
             echo $name . " ではありません。".PHP_EOL;
         }
     }
@@ -99,7 +101,7 @@ class NUMBER03
     {
         $total = 0;
 
-        for($i=0;$i<10000;$i++){
+        for ($i = 1; $i <= 10000; $i++) {
             $total +=  $i;
         }
     
@@ -116,15 +118,15 @@ class NUMBER03
             "すいか"
         ];
     
-        foreach($fruits as $fruit){
+        foreach ($fruits as $fruit) {
             echo $fruit.PHP_EOL;
         }
     }
 
     private function DivideFive()
     {
-        for($i=1;$i<=100;$i++){
-            if($i % 5 == 0){
+        for ($i = 1; $i <= 100; $i++) {
+            if ($i % 5 == 0) {
                 echo $i.PHP_EOL;
             }
         }
@@ -145,52 +147,129 @@ class NUMBER03
 
 class Number04
 {
-    private function double($num)
+    private function Double($num)
     {
         return $num * 2;
     }
 
-    private function sum($a,$b)
+    private function Sum($a,$b)
     {
         return $a+$b;
     }
 
+    private function Times($arr)
+    {
+        $total = 1;
+
+        foreach ($arr as $number) {
+            $total *= $number;
+        }
+
+        return $total;
+    }
+
     private function CreateTestArray()
     {
-        $array=array();
+        $array = array();
 
-        for($i=0;$i<5;$i++){
+        for ($i = 0; $i < 5; $i++) {
             $randnum = mt_rand(1,999);
+
             array_push($array,$randnum);
+
             echo $array[$i].PHP_EOL;
         }
 
-        foreach($array as $number){
+        foreach ($array as $number) {
             echo $number.PHP_EOL;
         }
+
         echo PHP_EOL;
+        
         return $array;
     }
 
     private function Arrays_Max($array)
     {
-        
         $maxnum = $array[0];
-        for($i=1;$i<=count($array);$i++){
-            if($maxnum < $array[$i]){
+        
+        for ($i = 1; $i <= count($array); $i++) {
+            if ($maxnum < $array[$i]) {
                 $maxnum = $array[$i];
             }
         }
+        
         return $maxnum;
+    }
+
+    private function BuiltInFunc()
+    {
+        // strip_tags
+        $str = "aaaaaa<?php echo 'aaa' ?>あいうえ<br>お";
+
+        echo "--strip_tags--".PHP_EOL;
+        echo $str.PHP_EOL;
+        echo strip_tags($str).PHP_EOL;
+
+        // array_push
+        echo PHP_EOL."--array_push--".PHP_EOL;
+        
+        $array = array(1,2,3,4,5);
+
+        foreach ($array as $object) {
+            echo $object.",";
+        }
+
+        echo PHP_EOL;
+        
+        array_push($array,6);
+
+        foreach ($array as $object) {
+            echo $object.",";
+        }
+
+        echo PHP_EOL;
+
+        // array_merge
+        echo PHP_EOL."--array_merge--".PHP_EOL;
+        
+        foreach ($array as $object) {
+            echo $object.",";
+        }
+        
+        echo PHP_EOL;
+
+        $array2 = array(7,8,9,10);
+        $array = array_merge($array,$array2);
+
+        foreach ($array as $object) {
+            echo $object.",";
+        }
+
+        echo PHP_EOL;
+
+        // time, mktime        // date
+        echo PHP_EOL."--time, mktime,date--".PHP_EOL;
+
+        echo date("Y-M-d-h-i-s",time()).PHP_EOL;
+        echo date("Y-M-d-h-i-s",mktime(12,13,59,4,21,1934)).PHP_EOL;
+        echo date(DATE_RFC2822).PHP_EOL;
     }
 
     public function RUN()
     {
-        echo $this->sum(1,2).PHP_EOL;
-        echo PHP_EOL;
-        echo $this->double(500).PHP_EOL;
+        echo $this->Double(500).PHP_EOL;
         echo PHP_EOL;  
+        echo $this->Sum(1,2).PHP_EOL;
+        echo PHP_EOL;
+
+        $array = array(1,3,5,7,9);
+
+        echo $this->Times($array).PHP_EOL;
+        echo PHP_EOL;
         echo $this->Arrays_Max($this->CreateTestArray($array)).PHP_EOL;
+        echo PHP_EOL;
+        echo $this->BuiltInFunc().PHP_EOL;
         echo PHP_EOL;
     }
 }
